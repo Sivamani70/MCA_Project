b7c2e97c73a42206bdc070bb6e2e15204622f919
sivamani70 <siva153335@gmail.com>
2020-09-02 13:04:56 +0530

    added age detection

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const video = document.getElementById('video')
+let predictedAges = [];
 
 // console.table(faceapi.nets)
 
@@ -6,8 +7,8 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.ageGenderNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models'),
 ])
   .then(startVideo)
   .catch((e) => console.log(`The Following Error was thrown during promise ${e}`))
@@ -40,13 +41,36 @@ video.addEventListener('play', () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions()
-      .withAgeAndGender()
       .withFaceDescriptors()
+      .withAgeAndGender()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    // faceapi.draw.drawTextField(`age:${WithAge.age} <br/>Gender:${WithGender.gen}`, canvas, resizedDetections)
+
+    const age = resizedDetections[0].age;
+    const interpolatedAge = await interpolateAgePredictions(age);
+    const bottomRight = {
+      x: resizedDetections[0].detection.box.bottomRight.x - 50,
+      y: resizedDetections[0].detection.box.bottomRight.y
+    };
+
+    // ${faceapi.Gender.MALE}
+    new faceapi.draw.DrawTextField(
+      [`${faceapi.round(interpolatedAge, 0)} years`],
+      bottomRight
+    ).draw(canvas);
+
+
+
   }, 100)
 })
+
+
+async function interpolateAgePredictions(age) {
+  predictedAges = [age].concat(predictedAges).slice(0, 30);
+  const avgPredictedAge = await
+    predictedAges.reduce((total, a) => total + a) / predictedAges.length;
+  return avgPredictedAge;
+}
